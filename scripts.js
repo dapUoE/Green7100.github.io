92bf442143f0216a66249682357fdaa7e9a13886
Green7100 <dannyprice2003@gmail.com>
2023-08-12 16:46:25 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,3 +21,37 @@ function toggleOverlayText(element, event) {
     event.stopPropagation();
     event.preventDefault();
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const prevButton = document.getElementById('prevChapter');
+    const nextButton = document.getElementById('nextChapter');
+    
+    const currentChapterNumber = getCurrentChapterNumber();
+
+    if (prevButton) {
+        prevButton.addEventListener('click', function() {
+            if (currentChapterNumber > 1) {
+                window.location.href = `arc8chapter${currentChapterNumber - 1}.html`;
+            }
+        });
+    }
+
+    if (nextButton) {
+        nextButton.addEventListener('click', function() {
+            window.location.href = `arc8chapter${currentChapterNumber + 1}.html`;
+        });
+    }
+});
+
+function getCurrentChapterNumber() {
+    const currentURL = window.location.pathname;
+    const chapterRegex = /arc8chapter(\d+)\.html/;
+    const match = currentURL.match(chapterRegex);
+    
+    if (match && match[1]) {
+        return parseInt(match[1], 10);
+    }
+    return 0;  // default, can be adjusted based on the structure
+}
+
